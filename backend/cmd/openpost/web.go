9c66f5fc25b99ae0e71fe5cf5f72ed1274ec65c7
Rodrigo Dias <kraktoos@gmail.com>
2026-04-08 20:53:11 +0100

    fix: serve index.html for all non-file routes

--- backend/cmd/openpost/web.go
+++ backend/cmd/openpost/web.go
@@ -4,6 +4,7 @@ import (
 	"embed"
 	"io/fs"
 	"net/http"
+	"os"
 	"strings"
 
 	"github.com/labstack/echo/v4"
@@ -15,14 +16,11 @@ import (
 //go:embed all:public
 var embeddedWeb embed.FS
 
-// RegisterSpaRoutes serves the SvelteKit SPA
 func RegisterSpaRoutes(e *echo.Echo) {
 	webFS, err := fs.Sub(embeddedWeb, "public")
 	if err != nil {
 		panic(err)
 	}
-
-	staticHandler := http.FileServer(http.FS(webFS))
 
 	e.GET("/*", func(c echo.Context) error {
 		reqPath := c.Request().URL.Path
@@ -31,22 +29,59 @@ func RegisterSpaRoutes(e *echo.Echo) {
 			return echo.NewHTTPError(http.StatusNotFound, "API not found")
 		}
 
-		staticHandler.ServeHTTP(c.Response().Writer, c.Request())
-		return nil
+		path := strings.TrimPrefix(reqPath, "/")
+
+		info, err := fs.Stat(webFS, path)
+		if err == nil {
+			if info.IsDir() {
+				indexPath := path
+				if indexPath != "" {
+					indexPath = indexPath + "/"
+				}
+				indexPath = indexPath + "index.html"
+
+				if _, err := fs.Stat(webFS, indexPath); err == nil {
+					indexData, err := fs.ReadFile(webFS, indexPath)
+					if err == nil {
+						c.Response().Header().Set("Content-Type", "text/html")
+						c.Response().Write(indexData)
+						return nil
+					}
+				}
+
+				return echo.NewHTTPError(http.StatusNotFound, "directory index not found")
+			}
+
+			file, err := webFS.Open(path)
+			if err != nil {
+				return echo.NewHTTPError(http.StatusNotFound, "file not found")
+			}
+			defer file.Close()
+
+			http.ServeContent(c.Response().Writer, c.Request(), info.Name(), info.ModTime(), file.(http.File))
+			return nil
+		}
+
+		if os.IsNotExist(err) {
+			indexData, err := fs.ReadFile(webFS, "index.html")
+			if err != nil {
+				return echo.NewHTTPError(http.StatusNotFound, "index.html not found")
+			}
+			c.Response().Header().Set("Content-Type", "text/html")
+			c.Response().Write(indexData)
+			return nil
+		}
+
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 	})
 
 	e.GET("/", func(c echo.Context) error {
-		return serveIndex(webFS, c)
+		indexData, err := fs.ReadFile(webFS, "index.html")
+		if err != nil {
+			return echo.NewHTTPError(http.StatusNotFound, "index.html not found")
+		}
+		c.Response().Header().Set("Content-Type", "text/html")
+		c.Response().Write(indexData)
+		return nil
 	})
-}
-
-func serveIndex(webFS fs.FS, c echo.Context) error {
-	indexData, err := fs.ReadFile(webFS, "index.html")
-	if err != nil {
-		return echo.NewHTTPError(http.StatusNotFound, "index.html not found")
-	}
-
-	c.Response().Header().Set("Content-Type", "text/html")
-	c.Response().Write(indexData)
-	return nil
 }
